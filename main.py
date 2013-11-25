cbb05b440864c42d507f376d3f44eb1a25a07759
neoocean <wjkim@neoocean.net>
2013-11-25 21:06:03 +0900

    메타데이터 변경과 인코딩을 한 번에 하게 바꿈. 아까처럼 음질이 한방에 이상해지지는 않는 듯. 근데 아이튠즈 라이브러리에 추가는 했지만 이게 내가 원하는 재생목록에 나타나지는 않는다.
    다음에 할 일은 낵 원하는 재생목록에 나타나는 것. 혹은 내가 지정한 아이튠즈의 지배를 받지 않는 디렉터리에 넣은 다음 이 경로 그대로 재생목록에 넣는 것. 지금 하는 것을 그대로 옮긴다면 후자가 되어야 하고 지금 이 커밋은 전자로 되어 있음.

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import commands
 import os
 import string
 from datetime import datetime
+import shutil
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #setting
 album = '걸어다니며 듣기'
-
+artist = '걸어다니며 듣기'
+genre = 'Voice'
 
 
 d = datetime.now()
@@ -23,7 +25,7 @@ filename = str(d.strftime('%Y%m%d-%H%M%S')) + '.aiff'
 # 제목과 내용을 묻는다.
 # title = raw_input('Title: ')
 # content = raw_input('Content: ')
-title = '제목:::::!'
+title = '새로운 제목:::::!'
 content = '내용'
 
 def escape_characters(s):
@@ -41,7 +43,6 @@ def escape_characters(s):
     s = string.replace(s, '@', '')
     s = string.replace(s, '$', '')
     s = string.replace(s, '^', '')
-    s = string.replace(s, ' ', '')
     return s
 
 title = escape_characters(title)
@@ -59,50 +60,45 @@ else:
     print 'Command: ' + cmd
 
 # 파일 이름을 내가 쓰는 규칙대로 바꾼다.
-new_filename = str(d.strftime('%Y%m%d')) + ' ' + \
+aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
     escape_characters(title) + '.aiff'
-os.rename(filename, new_filename)
-print 'Renamed as ' + new_filename
+os.rename(filename, aiff_filename)
+print 'Renamed as ' + aiff_filename
 
-# mp3 형식으로 컨버팅한다.
+
+# mp3 형식으로 컨버팅하는 ffmpeg 커맨드를 준비한다.
 mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
-cmd = './ffmpeg -y -i \'' + new_filename + \
-    '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050 \'' + mp3_filename + '\''
+cmd = './ffmpeg -y -i \'' + aiff_filename + \
+    '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050'
+
+# 메타데이터를 입력하는 ffmpeg 커맨드를 준비한다.
+# http://jonhall.info/how_to/create_id3_tags_using_ffmpeg
+cmd = cmd + ' -metadata artist=\'\''
+cmd = cmd + ' -metadata title=\'' + escape_characters(title) + '\''
+cmd = cmd + ' -metadata album=\'' + album + '\''
+cmd = cmd + ' -metadata artist=\'' + artist + '\''
+cmd = cmd + ' -metadata TIT1=\'' + album + '\'' # itunes groupping
+cmd = cmd + ' -metadata genre=\'' + genre + '\''
+
+# 커맨드 끝에 출력 파일 이름을 붙인다.
+cmd = cmd + ' \'' + mp3_filename + '\''
+
 result = commands.getstatusoutput(cmd)
 if result[0] == 0:
-    print 'Converted as ' + filename
+    print 'Converted as ' + mp3_filename
 else:
     print 'Error to converting. Code: ' + str(result[0])
     print 'Message: ' + str(result[1])
     print 'Command: ' + cmd
 
-# 메타데이터를 입력한다.
-def setMetadata(target, key, value):
-    cmd = './ffmpeg -y -i \'' + target + '\'' + \
-        ' -metadata ' + key + '=\'' + value + '\'' + \
-        ' \'' + target + '\''
-    result = commands.getstatusoutput(cmd)
-    if result[0] == 0:
-        print 'Metadata ' + key + ' saved to ' + mp3_filename
-    else:
-        print 'Error to write metadata. Code: ' + str(result[0])
-        print 'Message: ' + str(result[1])
-        print 'Command: ' + cmd        
-
-setMetadata(mp3_filename, 'artist', '')
-#setMetadata(mp3_filename, 'title', escape_characters(title))
-#setMetadata(mp3_filename, 'album', album)
-#setMetadata(mp3_filename, 'TIT1', album)
-#setMetadata(mp3_filename, 'genre', 'genre')
-
 # aiff 파일을 삭제한다.
 try:
-    os.remove(new_filename)
+    os.remove(aiff_filename)
 except OSError:
     pass
 
+# 파일을 아이튠즈에 등록한다. 아마 아이튠즈가 실행되어 있어야 할 듯.
+src = './' + mp3_filename
+dst = '/Users/neoocean/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized'
+shutil.move(src, dst)
 
-# 파일을 내가 쓰는 디렉토리로 옮긴다.
-# .. 이건 아직.
-
-# 지정된 아이튠즈 라이브러리에 등록한다. (오토메이터를 조금 사용해야 할까?)
