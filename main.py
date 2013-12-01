d62f85462c6799d304eac2421b063e1c5b649942
neoocean <wjkim@neoocean.net>
2013-12-01 18:01:51 +0900

    반복할 수 있도록 각 부분을 떼내서 함수처럼 만들었음. 하지만 이렇게 만들어놓고 보니 정말 거 참. :(

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def escape_characters(s):
 
 d = datetime.now()
 filename = str(d.strftime('%Y%m%d-%H%M%S')) + '.aiff'
+title = ''
+full_content = ''
 
 
 # 제목과 내용을 csv 파일로부터 읽는다.
@@ -67,63 +69,92 @@ with open('list.csv', 'rb') as c:
 sys.exit()
 
 
+convertToVoice(filename, full_content)
+def convertToVoice(filename, full_content):
+    # 커맨드를 실행해 파일로 만든다.
+    cmd = 'say -o ./\'' + filename + '\' \'' + full_content + '\''
+    result = commands.getstatusoutput(cmd)
+    if result[0] == 0:
+        print 'Converted as ' + filename
+    else:
+        print 'Error to converting. Code: ' + str(result[0])
+        print 'Message: ' + str(result[1])
+        print 'Command: ' + cmd
 
-# 커맨드를 실행해 파일로 만든다.
-cmd = 'say -o ./\'' + filename + '\' \'' + full_content + '\''
-result = commands.getstatusoutput(cmd)
-if result[0] == 0:
-    print 'Converted as ' + filename
-else:
-    print 'Error to converting. Code: ' + str(result[0])
-    print 'Message: ' + str(result[1])
-    print 'Command: ' + cmd
+renameVoice(title)
+def renameVoice(title):
+    # 파일 이름을 내가 쓰는 규칙대로 바꾼다.
+    aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
+        escape_characters(title) + '.aiff'
+    os.rename(filename, aiff_filename)
+    print 'Renamed as ' + aiff_filename
 
-# 파일 이름을 내가 쓰는 규칙대로 바꾼다.
-aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
-    escape_characters(title) + '.aiff'
-os.rename(filename, aiff_filename)
-print 'Renamed as ' + aiff_filename
+base_cmd = getFfmpegBaseCommand(title)
+def getFfmpegBaseCommand(title):
+    # mp3 형식으로 컨버팅하는 ffmpeg 커맨드를 준비한다.
+    mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
+    cmd = './ffmpeg -y -i \'' + aiff_filename + \
+        '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050'
+    return cmd
 
-
-# mp3 형식으로 컨버팅하는 ffmpeg 커맨드를 준비한다.
-mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
-cmd = './ffmpeg -y -i \'' + aiff_filename + \
-    '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050'
-
-# 메타데이터를 입력하는 ffmpeg 커맨드를 준비한다.
-# http://jonhall.info/how_to/create_id3_tags_using_ffmpeg
-cmd = cmd + ' -metadata artist=\'\''
-cmd = cmd + ' -metadata title=\'' + escape_characters(title) + '\''
-cmd = cmd + ' -metadata album=\'' + album + '\''
-cmd = cmd + ' -metadata artist=\'' + artist + '\''
-cmd = cmd + ' -metadata TIT1=\'' + album + '\'' # itunes groupping
-cmd = cmd + ' -metadata genre=\'' + genre + '\''
-cmd = cmd + ' -metadata TIT3=\'' + mp3_filename + '\''
+meta_cmd = getFfmpegMetaCommand(title, album, artist, genre, mp3_filename)
+def getFfmpegMetaCommand(title, album, artist, genre, mp3_filename):
+    # 메타데이터를 입력하는 ffmpeg 커맨드를 준비한다.
+    # http://jonhall.info/how_to/create_id3_tags_using_ffmpeg
+    cmd = ''
+    cmd = cmd + ' -metadata artist=\'\''
+    cmd = cmd + ' -metadata title=\'' + escape_characters(title) + '\''
+    cmd = cmd + ' -metadata album=\'' + album + '\''
+    cmd = cmd + ' -metadata artist=\'' + artist + '\''
+    cmd = cmd + ' -metadata TIT1=\'' + album + '\'' # itunes groupping
+    cmd = cmd + ' -metadata genre=\'' + genre + '\''
+    cmd = cmd + ' -metadata TIT3=\'' + mp3_filename + '\''
+    return cmd
 
 
 # 커맨드 끝에 출력 파일 이름을 붙인다.
+cmd = base_cmd + meta_cmd
 cmd = cmd + ' \'' + mp3_filename + '\''
 
-result = commands.getstatusoutput(cmd)
-if result[0] == 0:
-    print 'Converted as ' + mp3_filename
-else:
-    print 'Error to converting. Code: ' + str(result[0])
-    print 'Message: ' + str(result[1])
-    print 'Command: ' + cmd
+runFfmpegCommand(cmd)
+def runFfmpegCommand(cmd):
+    #ffmpeg 커맨드를 실행한다. 부디 같은 디렉토리 안에 있기를 바래요.
+    result = commands.getstatusoutput(cmd)
+    if result[0] == 0:
+        print 'Converted as ' + mp3_filename
+    else:
+        print 'Error to converting. Code: ' + str(result[0])
+        print 'Message: ' + str(result[1])
+        print 'Command: ' + cmd
 
-# aiff 파일을 삭제한다.
-try:
-    os.remove(aiff_filename)
-except OSError:
-    pass
 
-# 파일을 아이튠즈에 추가한다.
-src = './' + mp3_filename
-dst = '/Users/neoocean/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized'
-shutil.move(src, dst)
+removeAIFF(aiff_filename)
+def removeAIFF(aiff_filename):
+    # aiff 파일을 삭제한다.
+    try:
+        os.remove(aiff_filename)
+    except OSError:
+        pass
+
+
+addVoiceToItunesLibrary(mp3_filename)
+def addVoiceToItunesLibrary(mp3_filename):
+    # 파일을 아이튠즈에 추가한다.
+    src = './' + mp3_filename
+    # dist 경로를 얻어올 방법이 필요해요.
+    dist = '/Users/neoocean/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized'
+    shutil.move(src, dist)
 
 
 
 # 끝?
+
+
+
+
+
+
+
+
+
 
