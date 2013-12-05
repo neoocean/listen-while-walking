e7e9e653b323b2b2b5741c9ab5f1af1689971a16
neoocean <wjkim@neoocean.net>
2013-12-05 22:12:36 +0900

    반복 가능해짐. 하지만 실행할 때마다 모든 로우를 처리하려고 할 것이다. 지금부터 할일은 한 번 처리한 데이터를 다시 처리하지 않도록 하는 것. 아이튠즈에 등록하는 부분은 아직 주석 처리한 상태이다.
    문제는 내 패턴을 보니 구글문서도구에 스프레드시트를 만든 다음 이걸 csv 파일로 익스포트해서 사용하고 있다는 거다. 이게 문제가 뭐냐면 한 번 처리한 데이터에 표시할 방법이 없다는 것. 만약 매번 csv 파일을 익스포트 할 때마다 기존 로우를 모두 삭제하거나 기존 로우에 변환된 데이터라는 사실을 내가 직접 표시해주지 않으면 안되는 거다. 아니면 구글문서도구에 직접 접근할 방법을 찾거나. 이 스크립트가 대기 타면서 며칠 동안 걸어다니며 듣기를 업데이트하지 않고 있는데, 어지간하면 일단 돌아갈 수 있게 해놓고 다음을 도모하는 것이 좋지 않을까 하는 생각이다. 만약 이 스크립트가 자체적으로 한 번이라도 처리한 적이 있는 데이터를 몽땅 들고 있도록 하는 것도 방법이다. csv 파일을 읽어 데이터를 처리하려고 할 때 자신이 가지고 있는 기존 데이터와 비교해서 변환한 적이 있는지 확인하는 식으로. 일단 무식하게 csv 파일을 하나 만들어서 거기다 때려넣지뭐.

--- main.py
+++ main.py
@@ -25,6 +25,76 @@ iTunesPlayList = '걸어다니며 듣기'
 
 ##
 
+def convertToVoice(filename, full_content):
+    # 커맨드를 실행해 파일로 만든다.
+    cmd = 'say -o ./\'' + filename + '\' \'' + full_content + '\''
+    result = commands.getstatusoutput(cmd)
+    if result[0] == 0:
+        print 'Converted as ' + filename
+    else:
+        print 'Error to converting. Code: ' + str(result[0])
+        print 'Message: ' + str(result[1])
+        print 'Command: ' + cmd
+
+
+def renameVoice(title):
+    # 파일 이름을 내가 쓰는 규칙대로 바꾼다.
+    aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
+        escape_characters(title) + '.aiff'
+    os.rename(filename, aiff_filename)
+    print 'Renamed as ' + aiff_filename
+
+
+def getFfmpegBaseCommand(title, aiff_filename):
+    # mp3 형식으로 컨버팅하는 ffmpeg 커맨드를 준비한다.
+    mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
+    cmd = './ffmpeg -y -i \'' + aiff_filename + \
+        '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050'
+    return cmd
+
+
+def getFfmpegMetaCommand(title, album, artist, genre, mp3_filename):
+    # 메타데이터를 입력하는 ffmpeg 커맨드를 준비한다.
+    # http://jonhall.info/how_to/create_id3_tags_using_ffmpeg
+    cmd = ''
+    cmd = cmd + ' -metadata artist=\'\''
+    cmd = cmd + ' -metadata title=\'' + escape_characters(title) + '\''
+    cmd = cmd + ' -metadata album=\'' + album + '\''
+    cmd = cmd + ' -metadata artist=\'' + artist + '\''
+    cmd = cmd + ' -metadata TIT1=\'' + album + '\'' # itunes groupping
+    cmd = cmd + ' -metadata genre=\'' + genre + '\''
+    cmd = cmd + ' -metadata TIT3=\'' + mp3_filename + '\''
+    return cmd
+
+
+def runFfmpegCommand(cmd):
+    #ffmpeg 커맨드를 실행한다. 부디 같은 디렉토리 안에 있기를 바래요.
+    result = commands.getstatusoutput(cmd)
+    if result[0] == 0:
+        print 'Converted as ' + mp3_filename
+    else:
+        print 'Error to converting. Code: ' + str(result[0])
+        print 'Message: ' + str(result[1])
+        print 'Command: ' + cmd
+
+
+def removeAIFF(aiff_filename):
+    # aiff 파일을 삭제한다.
+    try:
+        os.remove(aiff_filename)
+        print 'Removed: ' + aiff_filename
+    except OSError:
+        pass
+
+
+def addVoiceToItunesLibrary(mp3_filename):
+    # 파일을 아이튠즈에 추가한다.
+    src = './' + mp3_filename
+    # dist 경로를 얻어올 방법이 필요해요.
+    dist = '/Users/neoocean/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized'
+    shutil.move(src, dist)
+
+
 
 def escape_characters(s):
     s = string.replace(s, ':', '')
@@ -40,6 +110,7 @@ def escape_characters(s):
     s = string.replace(s, '}', '')
     s = string.replace(s, '@', '')
     s = string.replace(s, '$', '')
+    s = string.replace(s, '?', '')
     s = string.replace(s, '^', '')
     s = string.replace(s, '"', '')
     s = string.replace(s, '\'', '')
@@ -56,95 +127,26 @@ full_content = ''
 # 제목과 내용을 csv 파일로부터 읽는다.
 with open('list.csv', 'rb') as c:
     reader = csv.reader(c)
-    next(reader, None)
+    next(reader, None) # skip Header Row.
+    count = 0
     for row in reader:
         if row[0] != '':
-            title = escape_characters(row[2])
-            full_content = title + '......' + row[3]
-            print title
-            print full_content
+            print 'Processing Row: ' + ++count
+            title = escape_characters(row[1])
+            full_content = title + '......' + row[2]
+            mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
+            aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
+                escape_characters(title) + '.aiff'
 
-
-# 어 잠깐. csv 읽기 전에 반복할 수 있게 만들어 놨어야지;
-sys.exit()
-
-
-convertToVoice(filename, full_content)
-def convertToVoice(filename, full_content):
-    # 커맨드를 실행해 파일로 만든다.
-    cmd = 'say -o ./\'' + filename + '\' \'' + full_content + '\''
-    result = commands.getstatusoutput(cmd)
-    if result[0] == 0:
-        print 'Converted as ' + filename
-    else:
-        print 'Error to converting. Code: ' + str(result[0])
-        print 'Message: ' + str(result[1])
-        print 'Command: ' + cmd
-
-renameVoice(title)
-def renameVoice(title):
-    # 파일 이름을 내가 쓰는 규칙대로 바꾼다.
-    aiff_filename = str(d.strftime('%Y%m%d')) + ' ' + \
-        escape_characters(title) + '.aiff'
-    os.rename(filename, aiff_filename)
-    print 'Renamed as ' + aiff_filename
-
-base_cmd = getFfmpegBaseCommand(title)
-def getFfmpegBaseCommand(title):
-    # mp3 형식으로 컨버팅하는 ffmpeg 커맨드를 준비한다.
-    mp3_filename = str(d.strftime('%Y%m%d')) + ' ' + title + '.mp3'
-    cmd = './ffmpeg -y -i \'' + aiff_filename + \
-        '\' -f mp3 -acodec libmp3lame -ab 48000 -ar 22050'
-    return cmd
-
-meta_cmd = getFfmpegMetaCommand(title, album, artist, genre, mp3_filename)
-def getFfmpegMetaCommand(title, album, artist, genre, mp3_filename):
-    # 메타데이터를 입력하는 ffmpeg 커맨드를 준비한다.
-    # http://jonhall.info/how_to/create_id3_tags_using_ffmpeg
-    cmd = ''
-    cmd = cmd + ' -metadata artist=\'\''
-    cmd = cmd + ' -metadata title=\'' + escape_characters(title) + '\''
-    cmd = cmd + ' -metadata album=\'' + album + '\''
-    cmd = cmd + ' -metadata artist=\'' + artist + '\''
-    cmd = cmd + ' -metadata TIT1=\'' + album + '\'' # itunes groupping
-    cmd = cmd + ' -metadata genre=\'' + genre + '\''
-    cmd = cmd + ' -metadata TIT3=\'' + mp3_filename + '\''
-    return cmd
-
-
-# 커맨드 끝에 출력 파일 이름을 붙인다.
-cmd = base_cmd + meta_cmd
-cmd = cmd + ' \'' + mp3_filename + '\''
-
-runFfmpegCommand(cmd)
-def runFfmpegCommand(cmd):
-    #ffmpeg 커맨드를 실행한다. 부디 같은 디렉토리 안에 있기를 바래요.
-    result = commands.getstatusoutput(cmd)
-    if result[0] == 0:
-        print 'Converted as ' + mp3_filename
-    else:
-        print 'Error to converting. Code: ' + str(result[0])
-        print 'Message: ' + str(result[1])
-        print 'Command: ' + cmd
-
-
-removeAIFF(aiff_filename)
-def removeAIFF(aiff_filename):
-    # aiff 파일을 삭제한다.
-    try:
-        os.remove(aiff_filename)
-    except OSError:
-        pass
-
-
-addVoiceToItunesLibrary(mp3_filename)
-def addVoiceToItunesLibrary(mp3_filename):
-    # 파일을 아이튠즈에 추가한다.
-    src = './' + mp3_filename
-    # dist 경로를 얻어올 방법이 필요해요.
-    dist = '/Users/neoocean/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized'
-    shutil.move(src, dist)
-
+            convertToVoice(filename, full_content)
+            renameVoice(title)
+            base_cmd = getFfmpegBaseCommand(title, aiff_filename)
+            meta_cmd = getFfmpegMetaCommand(title, album, artist, genre, mp3_filename)
+            cmd = base_cmd + meta_cmd
+            cmd = cmd + ' \'' + mp3_filename + '\''
+            runFfmpegCommand(cmd)
+            removeAIFF(aiff_filename)
+            # addVoiceToItunesLibrary(mp3_filename)
 
 
 # 끝?
