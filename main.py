fc55516d5636492d0d28b875967ce5cdd7b5addb
neoocean <wjkim@neoocean.net>
2013-12-08 23:14:32 +0900

    wordCorrect 데이터에 뉴스패퍼민트에 늘 나오는 한자 '마'를 추가.

--- main.py
+++ main.py
@@ -134,6 +134,8 @@ def correctWords(full_content):
     full_content = string.replace(full_content, '386용', '삼팔육용')
     full_content = string.replace(full_content, '386의', '삼팔육의')
     full_content = string.replace(full_content, '386은', '삼팔육은')
+    full_content = string.replace(full_content, '美', '미')
+
     # full_content = string.replace(full_content, '', '')
     return full_content
 
